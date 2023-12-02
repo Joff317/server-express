d0e10e38b79551bd3dd76a4d730ea120561e1ea0
Joff317 <j.freixanet@hotmail.com>
2023-12-02 12:08:56 +0100

    do search route

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const port = 3001;
 
 app.use(cors());
 
+// ---------------------------- GET ALL PLANTS BY PAGE ----------------------------
 app.get("/plants", async (req, res) => {
   try {
     const token = process.env.TOKEN;
     const page = req.query.page || 1;
-    const apiUrl = `https://trefle.io/api/v1/plants?token=${token}`;
+    const apiUrl = `https://trefle.io/api/v1/plants?token=${token}&page=1`;
 
     const response = await axios.get(apiUrl);
     res.json(response.data);
@@ -22,13 +23,32 @@ app.get("/plants", async (req, res) => {
       .json({ error: "Erreur lors de la récupération des données" });
   }
 });
+
+// ---------------------------- FILTER BY PARAMS ----------------------------
 app.get("/plants/filter", async (req, res) => {
   try {
     const token = process.env.TOKEN;
     const { filterParams, params } = req.query;
 
-    const apiUrl = `https://trefle.io/api/v1/plants?token=${token}&filter[${filterParams}]`;
-    // =${params}
+    const apiUrl = `https://trefle.io/api/v1/plants?token=${token}&filter[${filterParams}]=${params}`;
+    //
+    // https://trefle.io/api/v1/plants?token=bUqrMeoN_DsqyUuovxf_LYlChfVtmQtvavP804J-B9k&filter[common_name]=Evergreen%20oak
+
+    const response = await axios.get(apiUrl);
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des données" });
+  }
+});
+// ---------------------------- SEARCH BY PLANT ----------------------------
+app.get("/plants/search", async (req, res) => {
+  try {
+    const token = process.env.TOKEN;
+    const { query } = req.query;
+
+    const apiUrl = `https://trefle.io/api/v1/plants?token=${token}&search=${query}`;
     // https://trefle.io/api/v1/plants?token=bUqrMeoN_DsqyUuovxf_LYlChfVtmQtvavP804J-B9k&filter[common_name]=Evergreen%20oak
 
     const response = await axios.get(apiUrl);
